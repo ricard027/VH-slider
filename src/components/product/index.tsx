7b95f4acdd74d5696953c6f89e42cec3ddf03c51
Ricardo <ricardo_cabista@hotmail.com>
2023-12-03 19:18:51 -0300

    feat: create product component

---
+++ src/components/product/index.tsx
@@ -0,0 +1,29 @@
+import { FunctionComponent } from 'react'
+
+import { Product } from '../../types/product'
+
+import styles from './product.module.css'
+
+import { formatPrice } from '../../utils/formatprice'
+
+const ProductComponent: FunctionComponent<Product> = (product) => {
+  const haspromo = product.bestPrice < product.listPrice
+  return (
+    <div key={product.id} className={styles.slide}>
+      <div className={styles.slide_item}>
+        <img src={product.image} alt={`product ${product.productName}`} />
+      </div>
+      <div className={styles.slide_item}>
+        {haspromo && <p className={styles.promo}>OFERTA</p>}
+        <h2>{product.productName}</h2>
+        <div className={styles.item_price}>
+          <p>R${formatPrice(product.listPrice)}</p>
+          <p>
+            R${formatPrice(product.bestPrice)} {product.measurementUnit}
+          </p>
+        </div>
+      </div>
+    </div>
+  )
+}
+export default ProductComponent
---
+++ src/types/product.ts
@@ -0,0 +1,11 @@
+export interface Product {
+  id: string
+  bestPrice: number
+  brand: string
+  image: string
+  link: string
+  listPrice: number
+  measurementUnit: string
+  productId: string
+  productName: string
+}
