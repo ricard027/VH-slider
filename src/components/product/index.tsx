017bbddb5e6b79e5e9ba7d7a4d7f3339a50ab20d
Ricardo <ricardo_cabista@hotmail.com>
2023-12-04 11:07:54 -0300

    feat: create navigation in  product component slide

--- src/components/product/index.tsx
+++ src/components/product/index.tsx
@@ -5,8 +5,11 @@ import { Product } from '../../types/product'
 import styles from './product.module.css'
 
 import { formatPrice } from '../../utils/formatprice'
+import { useNavigate } from 'react-router-dom'
 
 const ProductComponent: FunctionComponent<Product> = (product) => {
+  const navigate = useNavigate()
+
   const haspromo = product.bestPrice < product.listPrice
   return (
     <div key={product.id} className={styles.slide}>
@@ -33,6 +36,13 @@ const ProductComponent: FunctionComponent<Product> = (product) => {
             R${formatPrice(product.bestPrice)} {product.measurementUnit}
           </p>
         </div>
+        <button
+          title={`Detalhes do produto ${product.productName}`}
+          className={styles.btn_navigate}
+          onClick={() => navigate(`/produto/${product.productId}`)}
+        >
+          Detalhes do produto
+        </button>
       </div>
     </div>
   )
