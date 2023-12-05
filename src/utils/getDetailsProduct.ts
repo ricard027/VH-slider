aefba31683ed5e13e677f64daacc40b33310d10b
Ricardo <ricardo_cabista@hotmail.com>
2023-12-05 11:40:20 -0300

    fix: files ununsed removed

--- src/utils/getDetailsProduct.ts
+++
@@ -1,9 +0,0 @@
-export const getDetailsProduct = async (id: string) => {
-  try {
-    const response = await fetch(`https://desafio.xlow.com.br/search/${id}`)
-    const data = response.json()
-    return data
-  } catch (error) {
-    console.log(error)
-  }
-}
